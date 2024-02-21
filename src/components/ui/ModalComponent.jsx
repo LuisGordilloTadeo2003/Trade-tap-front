7a2005756e50053f3d5135e95e9425743095578f
ismael <ismontelongo@outlook.com>
2024-02-21 17:12:34 +0000

    update: no es necesario el campo estado

--- src/components/ui/ModalComponent.jsx
+++ src/components/ui/ModalComponent.jsx
@@ -19,7 +19,6 @@ const ModalComponent = ({ showModal, handleCloseModal, nav, user}) => {
     let [titulo, setTitulo] = useState();
     const trabajador_id = user.id;
     const cliente_id = nav.id;
-    const estado = 'Pendiente';
     
 
     const enviarSolicitud = async () => {
@@ -28,11 +27,7 @@ const ModalComponent = ({ showModal, handleCloseModal, nav, user}) => {
             titulo,
             trabajador_id,
             cliente_id,
-            estado,
         };
-
-        console.log(payload);
-        console.log(xsrfToken);
         
         axios.defaults.headers['X-XSRF-TOKEN'] = xsrfToken;
 
