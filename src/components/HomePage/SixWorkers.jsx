f27e1dc2a5fcbf550ca0605ba5b181d1865d70b7
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-02-23 18:19:36 +0000

    six Workers

--- src/components/HomePage/SixWorkers.jsx
+++ src/components/HomePage/SixWorkers.jsx
@@ -1,47 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SixWorkerCard from "./SixWorkerCard";
 
-const SixWorkers = () => {
-    let workers = [];
+import axios from "../../lib/axios";
+import BigSpinner from "../ui/BigSpinner";
 
-    for (let i = 0; i < 6; i++) {
-        let worker = {
-            id: i,
-            nombre: "Luis",
-            apellido1: "Gordillo",
-            apellido2: "Tadeo",
-            profesion: "Programador",
-            valoracion: Math.floor(Math.random() * 5 + 1)
-        }
+const SixWorkers = ({ user }) => {
+    const [workers, setWorkers] = useState([]);
 
-        workers.push(worker);
+    const sixWorkers = async () => {
+        await axios.get(`api/trabajador?cerca=${user.localidad}`, {
+        })
+            .then(function (response) {
+                setWorkers(response.data.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    useEffect(() => {
+        sixWorkers();
+    }, [workers]);
+
+    console.log(user);
+
+    // for (let i = 0; i < 6; i++) {
+    //     let worker = {
+    //         id: i,
+    //         nombre: "Luis",
+    //         apellido1: "Gordillo",
+    //         apellido2: "Tadeo",
+    //         profesion: "Programador",
+    //         valoracion: Math.floor(Math.random() * 5 + 1)
+    //     }
+
+    //     workers.push(worker);
+    // }
+
+    if (workers.length == 0) {
+        return (
+            <BigSpinner />
+        )
     }
 
     return (
-        <div className="col-8">
-            <div className="row">
-                <div className="col-4">
-                    <SixWorkerCard key={workers[0].id} worker={workers[0]} />
-                </div>
-                <div className="col-4">
-                    <SixWorkerCard key={workers[1].id} worker={workers[1]} />
-                </div>
-                <div className="col-4">
-                    <SixWorkerCard key={workers[2].id} worker={workers[2]} />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-4">
-                    <SixWorkerCard key={workers[3].id} worker={workers[3]} />
-                </div>
-                <div className="col-4">
-                    <SixWorkerCard key={workers[4].id} worker={workers[4]} />
-                </div>
-                <div className="col-4">
-                    <SixWorkerCard key={workers[5].id} worker={workers[5]} />
-                </div>
-            </div>
-        </div>
+        <></>
+        // <div className="col-8">
+        //     <div className="row">
+        //         <div className="col-4">
+        //             <SixWorkerCard key={workers[0].id} worker={workers[0]} />
+        //         </div>
+        //         <div className="col-4">
+        //             <SixWorkerCard key={workers[1].id} worker={workers[1]} />
+        //         </div>
+        //         <div className="col-4">
+        //             <SixWorkerCard key={workers[2].id} worker={workers[2]} />
+        //         </div>
+        //     </div>
+        //     <div className="row">
+        //         <div className="col-4">
+        //             <SixWorkerCard key={workers[3].id} worker={workers[3]} />
+        //         </div>
+        //         <div className="col-4">
+        //             <SixWorkerCard key={workers[4].id} worker={workers[4]} />
+        //         </div>
+        //         <div className="col-4">
+        //             <SixWorkerCard key={workers[5].id} worker={workers[5]} />
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
